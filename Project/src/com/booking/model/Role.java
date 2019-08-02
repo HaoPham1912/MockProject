3e03eee13d3f3e8fc1fe4377695d64c97c0655ee
HaoPham1912 <nhuhaopham.spkt@gmail.com>
2019-08-02 11:07:19 +0700

    model class

---
+++ Project/src/com/booking/model/Role.java
@@ -0,0 +1,18 @@
+package com.booking.model;
+
+public class Role {
+	private int id_role;
+	private String rolename;
+	public int getId_role() {
+		return id_role;
+	}
+	public void setId_role(int id_role) {
+		this.id_role = id_role;
+	}
+	public String getRolename() {
+		return rolename;
+	}
+	public void setRolename(String rolename) {
+		this.rolename = rolename;
+	}
+}	
