209862b31cee400da8b4941fea3e45ee72dd9a59
HaoPham1912 <nhuhaopham.spkt@gmail.com>
2019-08-04 16:25:20 +0700

    update 2 register

--- MockProject/src/com/booking/service/ICustomerService.java
+++ MockProject/src/com/booking/service/ICustomerService.java
@@ -4,4 +4,5 @@ import com.booking.model.Account;
 
 public interface ICustomerService {
 	Account FindAccountByUsernamePassword(String username, String password);
+	void insertInfoCustomer(int id, String name, String phone, String email, String address);
 }
