86d598e208c26b18081d0aecc61d49e92b01b415
HaoPham1912 <nhuhaopham.spkt@gmail.com>
2019-08-19 10:43:52 +0700

    asd

--- MockProject/src/com/booking/controller/CustomTicketServlet.java
+++ MockProject/src/com/booking/controller/CustomTicketServlet.java
@@ -68,7 +68,7 @@ public class CustomTicketServlet extends HttpServlet {
 			{
 				HttpSession session = request.getSession();
 				String id_bus = (String) session.getAttribute("id_bus");
-				session.setAttribute("DeleteTicketFailed", "Cannot delete this ticket");
+				session.setAttribute("DeleteTicketFail", "Cannot delete this ticket");
 				response.sendRedirect(request.getContextPath()+"/emp-filterTicket?id_bus="+id_bus);
 			}
 		}
